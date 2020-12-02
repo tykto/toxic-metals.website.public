ebbae48f687aa9d3b9fb763befdfa9d772e5fa9a
Lara Robertson <laraejrobertson@gmail.com>
2020-12-03 09:42:58 +1100

    refactor: section 3 styles

--- src/routes/_components/section-3.svelte
+++ src/routes/_components/section-3.svelte
@@ -2,9 +2,47 @@
 </script>
 
 <style>
+  .image {
+    background: url(/images/city-bg.jpg) no-repeat center center;
+    background-size: cover;
+    min-height: 600px;
+  }
+
+  .panel {
+    background-color: #ffffff;
+    border-left: 10px solid #a9e5e5;
+    border-right: 0 solid transparent;
+    border-top: 0 solid transparent;
+    border-bottom: 0 solid transparent;
+    border-radius: 0px;
+    box-shadow: none;
+    padding: 15px;
+    color: #000000;
+    margin: 0;
+    min-height: 600px;
+    display: table;
+  }
+
+  @media (max-width: 768px) {
+    .image {
+      min-height: 400px;
+    }
+    .panel {
+      min-height: 400px;
+    }
+  }
+
+  @media (max-width: 767px) {
+    .panel {
+      border-right: 0 solid transparent;
+      border-left: 0 solid transparent;
+      border-top: 10px solid #a9e5e5;
+      border-bottom: 0 solid transparent;
+    }
+  }
 </style>
 
-<div class="content-section-3">
+<div>
   <div class="container-fluid">
     <div class="row no-gutter">
       <div class="col-sm-6">
