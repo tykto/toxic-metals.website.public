7ae7fd0d7fd3d48006d9ab708bfd6e32847e4eaf
Lara Robertson <laraejrobertson@gmail.com>
2020-12-02 15:47:41 +1100

    feat: tidy svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,178 +5,185 @@
 </style>
 
 <svelte:head>
-	<title>Toxic Metal Detox Clinic, Melbourne</title>
+  <title>Toxic Metal Detox Clinic, Melbourne</title>
 </svelte:head>
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container-fluid topnav">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#"> </a>
-				<h1 class="title text-hide">Toxic Metal Detox Clinic, Melbourne</h1>
-				<div class="contact">
-					<p>Janine Brundle</p>
-					<p>PD Hom(UK) NA MAROH MAHA</p>
-					<p class="number">P: 03 9448 2334</p>
-				</div>
-            </div>
-        </div>
-    </nav>
-    <div class="intro-header">
-		<div class="container">
-			<div class="row text-center">
-				<h1 class="tag-1">RESTORING HEALTH </h1>
-				<h1 class="tag-2">CREATING BALANCE</h1>
-			</div>
-			<div class="row">
-				<div class="col-sm-6">
-					<img class="img-responsive" src="images/silver-frame-o-2.jpg" alt="frame">
-				</div>
-				<div class="col-sm-6">
-					<div class="clinic-tag text-center">
-						<p>A specialised clinic focusing on gentle, safe, natural, supportive and individualised heavy metal detoxing</p>
-					</div>
-				</div>
-			</div>
-		</div>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container-fluid topnav">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">&nbsp;</a>
+      <h1 class="title text-hide">Toxic Metal Detox Clinic, Melbourne</h1>
+      <div class="contact">
+        <p>Janine Brundle</p>
+        <p>PD Hom(UK) NA MAROH MAHA</p>
+        <p class="number">P: 03 9448 2334</p>
+      </div>
     </div>
-    <div class="content-section-1">
-        <div class="container-fluid">
-            <div class="row no-gutter">
-				<div class="col-sm-6">
-                    <div class="image">
-					</div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="clearfix"></div>
-					<div class="panel panel-default">
-						<div class="vcenter">
-							<div class="panel-body center-block">
-								<ul class="fa-ul">
-									<li><i class="fa-li fa fa-check"></i>Life changing positive answers to your conditions</li>
-									<li><i class="fa-li fa fa-check"></i>Empowerment with knowledge and better health</li>
-									<li><i class="fa-li fa fa-check"></i>Treating the cause, not the symptoms</li>
-									<li><i class="fa-li fa fa-check"></i>Discover what is causing your unexplained health problems with an instant Heavy Metal Test</li>
-								<ul>
-							</div>
-						</div>
-					</div>
-                </div>
-            </div>
-        </div>
+  </div>
+</nav>
+<div class="intro-header">
+  <div class="container">
+    <div class="row text-center">
+      <h1 class="tag-1">RESTORING HEALTH</h1>
+      <h1 class="tag-2">CREATING BALANCE</h1>
     </div>
-	<div class="content-section-2">
-        <div class="container-fluid">
-            <div class="row no-gutter">
-				<div class="col-sm-6 col-sm-push-6">
-                    <div class="image">
-					</div>
-                </div>
-                <div class="col-sm-6 col-sm-pull-6">
-                    <div class="clearfix"></div>
-					<div class="panel panel-default">
-						<div class="vcenter">
-							<div class="panel-body center-block">
-								<ul class="fa-ul">
-									<li>Did you know that minerals are essential to life and that toxic metals prevent their absorption in the body</li>
-									<li>Heavy metal toxicity and essential mineral deficiencies are a major cause of chronic illness for many Australians.  In most cases this underlying cause goes undetected or even worse, untreated</li>
-									<li>Restoring health & creating balance  with gentle, safe, natural supportive and individualized heavy metal detoxing</li>
-									<li>Providing positive answers to chronic symptoms from a different perspective</li>
-								<ul>
-							</div>
-						</div>
-					</div>
-                </div>
-            </div>
+    <div class="row">
+      <div class="col-sm-6"><img class="img-responsive" src="images/silver-frame-o-2.jpg" alt="frame" /></div>
+      <div class="col-sm-6">
+        <div class="clinic-tag text-center">
+          <p>A specialised clinic focusing on gentle, safe, natural, supportive and individualised heavy metal detoxing</p>
         </div>
+      </div>
     </div>
-    <div class="content-section-3">
-        <div class="container-fluid">
-			<div class="row no-gutter">
-                <div class="col-sm-6">
-                    <div class="image">
-					</div>
-                </div>
-				<div class="col-sm-6">
-                    <div class="clearfix"></div>
-                    <div class="panel panel-default">
-						<div class="vcenter">
-							<div class="panel-body">	
-								<p>Everybody has heavy toxic metals in their body,  they are in our daily environment and over time they can accumulate in the body causing serious chronic illnesses. 
-								</p>
-								<p>Today it is not a question of whether or not you have been exposed to heavy metals, but rather by which ones and by how much.
-								</p>
-							</div>
-						</div>
-					</div>
-                </div>
+  </div>
+</div>
+<div class="content-section-1">
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="col-sm-6">
+        <div class="image" />
+      </div>
+      <div class="col-sm-6">
+        <div class="clearfix" />
+        <div class="panel panel-default">
+          <div class="vcenter">
+            <div class="panel-body center-block">
+              <ul class="fa-ul">
+                <li><i class="fa-li fa fa-check" />Life changing positive answers to your conditions</li>
+                <li><i class="fa-li fa fa-check" />Empowerment with knowledge and better health</li>
+                <li><i class="fa-li fa fa-check" />Treating the cause, not the symptoms</li>
+                <li><i class="fa-li fa fa-check" />Discover what is causing your unexplained health problems with an instant Heavy Metal Test</li>
+                <ul />
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-	<div class="content-section-5">
-        <div class="container-fluid">
-			<div class="row no-gutter">
-                <div class="col-sm-6 col-sm-push-6">
-                    <div class="image">
-					</div>
-                </div>
-				<div class="col-sm-6 col-sm-pull-6">
-                    <div class="clearfix"></div>
-                    <div class="panel panel-default">
-						<div class="vcenter">
-							<div class="panel-body">
-								<p>I use the most up to date testing methods (both painless and no blood is drawn) providing you with instant and accurate results of  toxic metals and mineral deficiencies in your body allowing me to tailor your safe, gentle, supportive and individualized detox program immediately.
-								</p>
-							</div>
-						</div>
-					</div>
-                </div>
+  </div>
+</div>
+<div class="content-section-2">
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="col-sm-6 col-sm-push-6">
+        <div class="image" />
+      </div>
+      <div class="col-sm-6 col-sm-pull-6">
+        <div class="clearfix" />
+        <div class="panel panel-default">
+          <div class="vcenter">
+            <div class="panel-body center-block">
+              <ul class="fa-ul">
+                <li>Did you know that minerals are essential to life and that toxic metals prevent their absorption in the body</li>
+                <li>
+                  Heavy metal toxicity and essential mineral deficiencies are a major cause of chronic illness for many Australians. In most cases this
+                  underlying cause goes undetected or even worse, untreated
+                </li>
+                <li>Restoring health & creating balance with gentle, safe, natural supportive and individualized heavy metal detoxing</li>
+                <li>Providing positive answers to chronic symptoms from a different perspective</li>
+                <ul />
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-    <div class="content-section-6">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-6 ">
-					<div class="janine-info">
-						<img class="img-responsive" src="images/janine.jpg" alt="janine">
-						<h3 class="name">Janine Brundle</h3>
-						<p class="qualifications">PD Hom(UK) NA MAROH MAHA</p>
-						<h4 class="titles">Specialist Clinician, Founder and CEO</h4>
-					</div>
-					<div class="clinic-info">
-						<h3 class="clinic">Toxic Metal Detox Clinic, Melbourne</h3>
-						<p>Clinic Room:</p>
-						<p>Unit 1, 25 Progress Street, Mornington Vic 3931</p>
-						<p class="c-number">P: 9448 2334</p>
-						<p class="c-number">M: 0419 836370</p>						
-						<p>E:
-							<a target="_blank" href="mailto:janine.brundle@bigpond.com?subject=web%20enquiry">janine@toxicmetals.com.au</a>
-						</p>
-					</div>
-					<div class="professional-logos">
-						<img class="img-responsive" src="images/minnd.png" alt="minnd">
-						<img class="img-responsive" src="images/iict.png" alt="iict">
-						<img class="img-responsive" src="images/homeopathy.jpg" alt="homeopathy">
-						<a target="_blank" href="https://au.linkedin.com/in/janine-brundle-p-d-hom-uk-na-maroh-maha-31896144"> <img class="img-responsive" src="images/linkedin.png" alt="linkedin"></a>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-6 map">
-					<div class="embed-responsive embed-responsive-16by9">
-						<iframe title="" class="embed-responsive-item" height="258" name="Toxic Metal Detox Clinic, Melbourne" scrolling="yes" src="https://maps.google.com/maps?hl=en&amp;q=1/25 Progress Street, Mornington, 3931&amp;ie=UTF8&amp;t=roadmap&amp;z=17&amp;iwloc=B&amp;output=embed" id="Toxic Metal Detox Clinic, Melbourne"></iframe>
-					</div>
-				</div>
+  </div>
+</div>
+<div class="content-section-3">
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="col-sm-6">
+        <div class="image" />
+      </div>
+      <div class="col-sm-6">
+        <div class="clearfix" />
+        <div class="panel panel-default">
+          <div class="vcenter">
+            <div class="panel-body">
+              <p>
+                Everybody has heavy toxic metals in their body, they are in our daily environment and over time they can accumulate in the body causing serious
+                chronic illnesses.
+              </p>
+              <p>Today it is not a question of whether or not you have been exposed to heavy metals, but rather by which ones and by how much.</p>
             </div>
-
+          </div>
         </div>
+      </div>
     </div>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-6">
-					<p class="text-left">&copy; Janine Brundle 2016</p>
-				</div>
-				<div class="col-xs-6">
-					<p class="text-right"><a target="_blank" href="http://www.tykto.com">Website by Tykto</a></p>
-				</div>
-			</div>
+  </div>
+</div>
+<div class="content-section-5">
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="col-sm-6 col-sm-push-6">
+        <div class="image" />
+      </div>
+      <div class="col-sm-6 col-sm-pull-6">
+        <div class="clearfix" />
+        <div class="panel panel-default">
+          <div class="vcenter">
+            <div class="panel-body">
+              <p>
+                I use the most up to date testing methods (both painless and no blood is drawn) providing you with instant and accurate results of toxic metals
+                and mineral deficiencies in your body allowing me to tailor your safe, gentle, supportive and individualized detox program immediately.
+              </p>
+            </div>
+          </div>
         </div>
-    </footer>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="content-section-6">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-md-6 ">
+        <div class="janine-info">
+          <img class="img-responsive" src="images/janine.jpg" alt="janine" />
+          <h3 class="name">Janine Brundle</h3>
+          <p class="qualifications">PD Hom(UK) NA MAROH MAHA</p>
+          <h4 class="titles">Specialist Clinician, Founder and CEO</h4>
+        </div>
+        <div class="clinic-info">
+          <h3 class="clinic">Toxic Metal Detox Clinic, Melbourne</h3>
+          <p>Clinic Room:</p>
+          <p>Unit 1, 25 Progress Street, Mornington Vic 3931</p>
+          <p class="c-number">P: 9448 2334</p>
+          <p class="c-number">M: 0419 836370</p>
+          <p>E: <a target="_blank" href="mailto:janine.brundle@bigpond.com?subject=web%20enquiry">janine@toxicmetals.com.au</a></p>
+        </div>
+        <div class="professional-logos">
+          <img class="img-responsive" src="images/minnd.png" alt="minnd" />
+          <img class="img-responsive" src="images/iict.png" alt="iict" />
+          <img class="img-responsive" src="images/homeopathy.jpg" alt="homeopathy" />
+          <a target="_blank" href="https://au.linkedin.com/in/janine-brundle-p-d-hom-uk-na-maroh-maha-31896144">
+            <img class="img-responsive" src="images/linkedin.png" alt="linkedin" /></a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-6 map">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe
+            title=""
+            class="embed-responsive-item"
+            height="258"
+            name="Toxic Metal Detox Clinic, Melbourne"
+            scrolling="yes"
+            src="https://maps.google.com/maps?hl=en&amp;q=1/25 Progress Street, Mornington, 3931&amp;ie=UTF8&amp;t=roadmap&amp;z=17&amp;iwloc=B&amp;output=embed"
+            id="Toxic Metal Detox Clinic, Melbourne" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-6">
+        <p class="text-left">&copy; Janine Brundle 2016</p>
+      </div>
+      <div class="col-xs-6">
+        <p class="text-right"><a target="_blank" href="http://www.tykto.com">Website by Tykto</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
