de686e653efa1101fd22ba3c44e4a883e0bc14a2
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-04 10:44:35 +1100

    refactor: ad layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,4 @@
   import Home from '@app/pages/Home';
 </script>
 
-<svelte:head>
-  <meta name="description" content="Toxic Metal Detox Clinic, Melbourne" />
-  <meta name="author" content="Janine Brundle" />
-  <meta
-    name="keywords"
-    content="Janine Brundle, Toxic Metal Detox Clinic, Melbourne, Bentleigh, detoxifying toxic metals, detox, heavy metals, oligoscan, heavy metal analysis,  spectrophotometry, heavy metal detoxing" />
-  <meta name="robots" content="index, follow" />
-  <title>Toxic Metal Detox Clinic, Melbourne</title>
-</svelte:head>
-
 <Home />
