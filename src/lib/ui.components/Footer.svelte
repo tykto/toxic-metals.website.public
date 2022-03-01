1ebc3b24bb41ceb5e4257439cb949223aab2eb22
Andrew Robertson <andrewcrobertson@gmail.com>
2022-03-01 16:29:02 +1100

    feat: update copyright to 2022

--- src/lib/ui.components/Footer.svelte
+++ src/lib/ui.components/Footer.svelte
@@ -3,7 +3,7 @@
 
 <footer class="font-cg1 bg-napa">
   <div class="container flex items-center h-8 md:h-10 lg:h-16 text-xs mx-auto p-2">
-    <span class="font-bold">&copy; Janine Brundle 2016</span>
+    <span class="font-bold">&copy; Janine Brundle 2022</span>
     <a class="text-port font-bold no-underline hover:text-white ml-auto" target="_blank" href="http://www.tykto.com">Website by Tykto</a>
   </div>
 </footer>
