4a1f734258ec89aff1ea7900b285dad0397cde03
Lara Robertson <laraejrobertson@gmail.com>
2022-03-03 10:32:37 +1100

    fix: align check marks with text

--- src/lib/ui.components/Section-1.svelte
+++ src/lib/ui.components/Section-1.svelte
@@ -7,11 +7,11 @@
       class="flex flex-1 justify-center items-center w-full h-96 lg:h-144 bg-watermark bg-no-repeat bg-right-bottom border-t-10 border-boston lg:border-t-0 lg:border-l-10"
     >
       <ul class="fa-ul w-4/5 leading-normal text-lg md:text-xl lg:text-2xl font-bold ">
-        <li><i class="fa-li fa fa-check" />Life changing positive answers to your conditions</li>
-        <li><i class="fa-li fa fa-check" />Empowerment with knowledge and better health</li>
-        <li><i class="fa-li fa fa-check" />Treating the cause, not the symptoms</li>
+        <li><span class="fa-li"><i class="fa fa-check" /></span>Life changing positive answers to your conditions</li>
+        <li><span class="fa-li"><i class="fa fa-check" /></span>Empowerment with knowledge and better health</li>
+        <li><span class="fa-li"><i class="fa fa-check" /></span>Treating the cause, not the symptoms</li>
         <li>
-          <i class="fa-li fa fa-check" />Discover what is causing your unexplained health problems with an instant Heavy Metal Test
+          <span class="fa-li"><i class="fa fa-check" /></span>Discover what is causing your unexplained health problems with an instant Heavy Metal Test
         </li>
       </ul>
     </div>
