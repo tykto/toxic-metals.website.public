b999048b98ee90229d063f39467973de1d39a3f3
Lara Robertson <laraejrobertson@gmail.com>
2022-07-13 13:07:21 +1000

    chore: upgrade svelte kit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-
   theme: {
     extend: {
       colors: {
@@ -81,8 +81,5 @@ const config = {
       },
     },
   },
-
   plugins: [],
-};
-
-module.exports = config;
+}
