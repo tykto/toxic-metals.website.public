4449eefcc6df32b4901d2f925f9283f4b45036bc
Lara Robertson <laraejrobertson@gmail.com>
2022-07-14 11:26:47 +1000

    feat: fixed section overlap issue

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,35 +1,35 @@
-// import type { Handle } from '@sveltejs/kit';
-// import type { Options } from 'html-minifier';
-// import { minify } from 'html-minifier';
+import type { Handle } from '@sveltejs/kit';
+import type { Options } from 'html-minifier';
+import { minify } from 'html-minifier';
 
-// const options: Options = {
-//   collapseBooleanAttributes: true,
-//   collapseWhitespace: true,
-//   conservativeCollapse: true,
-//   decodeEntities: true,
-//   html5: true,
-//   ignoreCustomComments: [/^#/],
-//   minifyCSS: true,
-//   minifyJS: true,
-//   removeAttributeQuotes: true,
-//   removeComments: true,
-//   removeOptionalTags: true,
-//   removeRedundantAttributes: true,
-//   removeScriptTypeAttributes: true,
-//   removeStyleLinkTypeAttributes: true,
-//   sortAttributes: true,
-//   sortClassName: true,
-//   removeEmptyElements: true,
-// };
+const options: Options = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+  decodeEntities: true,
+  html5: true,
+  ignoreCustomComments: [/^#/],
+  minifyCSS: true,
+  minifyJS: true,
+  removeAttributeQuotes: true,
+  removeComments: true,
+  removeOptionalTags: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  sortAttributes: true,
+  sortClassName: true,
+  removeEmptyElements: true,
+};
 
-// export const handle: Handle = async ({ event, resolve }) => {
-//   const response = await resolve(event);
+export const handle: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event);
 
-//   if (response.headers.get('content-type') === 'text/html') {
-//     const body = await response.text();
-//     const minifiedBody = minify(body, options);
-//     return new Response(minifiedBody);
-//   } else {
-//     return response;
-//   }
-// };
+  if (response.headers.get('content-type') === 'text/html') {
+    const body = await response.text();
+    const minifiedBody = minify(body, options);
+    return new Response(minifiedBody);
+  } else {
+    return response;
+  }
+};
